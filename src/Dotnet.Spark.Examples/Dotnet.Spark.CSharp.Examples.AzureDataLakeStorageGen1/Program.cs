52a93e87c577db727d6eeae1c7e85ee2fe21fa89
Usman Mohammed <usman.mohammed.17@aberdeen.ac.uk>
2020-08-17 00:02:26 +0100

    add read arguments from console

--- src/Dotnet.Spark.Examples/Dotnet.Spark.CSharp.Examples.AzureDataLakeStorageGen1/Program.cs
+++ src/Dotnet.Spark.Examples/Dotnet.Spark.CSharp.Examples.AzureDataLakeStorageGen1/Program.cs
@@ -10,26 +10,25 @@ namespace Dotnet.Spark.CSharp.Examples.AzureDataLakeStorageGen1
         static void Main(string[] args)
         {
             // Verify environment variables
-            // if (args.Length != 2)
-            // {
-            //     Console.Error.WriteLine("Usage: $ADLS_SP_CLIENT_ID $ADLS_SP_CLIENT_SECRET");
-            //     Environment.Exit(1);
-            // }
+            if (args.Length != 3)
+            {
+                Console.Error.WriteLine("Usage: $TENANT_ID $ADLS_NAME $ADLS_SP_CLIENT_ID $ADLS_SP_CLIENT_SECRET");
+                Environment.Exit(1);
+            }
 
-            // Specify file path in Azure Storage
+            // Specify file path in Azure Data Lake Gen 1
             string filePath =
-                $"adl://dynamicdevdemo.azuredatalakestore.net/json/people.json";
+                $"adl://{args[1]}.azuredatalakestore.net/json/people.json";
 
             // Create SparkSession
             SparkSession spark = SparkSession
                 .Builder()
                 .AppName("Azure Storage example using .NET for Apache Spark")
                 .Config("fs.adl.impl", "org.apache.hadoop.fs.adl.AdlFileSystem")
-                .Config("fs.AbstractFileSystem.adl.impl", "org.apache.hadoop.fs.adl.Adl")
                 .Config("fs.adl.oauth2.access.token.provider.type", "ClientCredential")
-                .Config("fs.adl.oauth2.client.id", "")
-                .Config("fs.adl.oauth2.credential", "")
-                .Config("fs.adl.oauth2.refresh.url", "https://login.microsoftonline.com/<directory-id>/oauth2/token")
+                .Config("fs.adl.oauth2.client.id", args[2])
+                .Config("fs.adl.oauth2.credential", args[3])
+                .Config("fs.adl.oauth2.refresh.url", $"https://login.microsoftonline.com/{args[0]}/oauth2/token")
                 .GetOrCreate();
 
             // Create sample data
@@ -53,10 +52,10 @@ namespace Dotnet.Spark.CSharp.Examples.AzureDataLakeStorageGen1
             // Print DataFrame
             df.Show();
 
-            // Write DataFrame to Azure Storage
+            // Write DataFrame to Azure Data Lake Gen 1
             df.Write().Mode(SaveMode.Overwrite).Json(filePath);
 
-            // Read saved DataFrame from Azure Storage
+            // Read saved DataFrame from Azure Data Lake Gen 1
             DataFrame readDf = spark.Read().Json(filePath);
 
             // Print DataFrame
